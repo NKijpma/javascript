42fff0d131923aadcff5dca53fe5de18ee176d49
NKijpma <Nikhilijpma11@gmail.com>
2026-03-05 09:42:03 +0100

    updated code images js and css

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--    test messafe als je dit ziet dan is ie geuodate-->
+    <meta charset="UTF-8">
+    <title>JavaScript </title>
+    <!-- css -->
+    <link rel="stylesheet" href="css/root.css"
+    <link rel="stylesheet" href="css/website.css">
+    <link rel="stylesheet" href="css/main.css">
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+<!--opgave 1 t/m 4-->
+<div class="topbar">
+    <h1> JavaScript opgaven 1 t/m 4 </h1>
+</div>
+<div id="box_container">
+    <div id="box1" class="box"></div>
+    <div id="box2" class="box"></div>
+    <img src="images/laila.jpg" alt="lila" id="laila" class="box">
+    <div id="cat_box" class="box"></div>
+</div>
+<!-- js -->
+<script src="js/website.js"></script>
+</body>
+</html>
--- js/website.js
+++ js/website.js
@@ -1,5 +1,5 @@
 document.querySelector('#box1').style.backgroundColor = "green";
-document.querySelector('#box1').style.borderRadius = "100%";
+document.querySelector('#box1').style.borderRadius = "20%";
 document.querySelector('#box2').style.backgroundColor = "red";
 document.querySelector('#box2').style.borderRadius = "10%";
 
@@ -7,18 +7,59 @@ window.addEventListener('load', function () {
     console.log("wat doe je in console lil vro !")
 });
 
-document.querySelector('#box1').addEventListener('click', function(){
+document.querySelector('#box1').addEventListener('click', function () {
 
-    document.querySelector('#box2').innerHTML = '<h1> Hello World !</h1>';
+    document.querySelector('#box2').innerHTML = '<h2> Hello World !</h2>';
 });
 
-document.querySelector('#box2').addEventListener('click', function(){
+// on box 2 click change color bpx1
+document.querySelector('#box2').addEventListener('click', function () {
+    document.querySelector('#box1').style.backgroundColor = "darkblue";
+});
 
-    console.log("wat klik je op box2 !")
+let cat = {
+    "naam": "laila ",
+    "leeftijd": "idk 1 denk ",
+    "hobbys": [
+        "slapen ",
+        "eten ",
+        "meowen "
+    ]
+};
+// on laila box click change content box1 to let cat
+document.querySelector('#laila').addEventListener('click', function () {
+    document.querySelector('#box1').innerHTML = `
+        <p>
+            Naam: ${cat.naam}<br>
+            Leeftijd: ${cat.leeftijd}<br>
+            Hobbys: ${cat.hobbys.join(', ')}
+        </p>
+    `;
 });
 
 
+// box that gets a random cat image from a api
+let catBox = document.querySelector('#cat_box');
+catBox.addEventListener('click', async function () {
 
-// document.querySelector('#search_form').addEventListener('submit', function() {
-//     console.log("Laten we dan nu het hele formulier naar PHP sturen.");
-// })
+    try {
+        let response = await fetch("https://api.thecatapi.com/v1/images/search", {
+            method: 'GET',
+            headers: {
+                "x-api-key": "live_eEhHHZcMeM8daSSoZGwsjTz5T932kdgLiValkNsALlwdL475xuTLjlSpTzormKTR"
+            }
+        });
+
+
+        let data = await response.json();
+        let img = document.createElement("img");
+        img.src = data[0].url;
+
+        catBox.innerHTML = "";
+        catBox.appendChild(img);
+    } catch (error) {
+        console.log(error);
+        document.querySelector('#cat_box').innerHTML =  'image not found,' +
+            '(waarschijnlijk premium image klik opnieuw)';
+    }
+});
